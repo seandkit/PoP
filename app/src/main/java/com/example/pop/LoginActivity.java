851ca4f6c71405fb2320647e23995b743eb63cd1
Adrian Collins <d00194311@student.dkit.ie>
2020-01-18 13:39:52 +0000

    Revert "Merge branch 'Develop' of https://github.com/seandkit/PoP into NFC"
    This reverts commit 3677d24682ae199986dcbebce18f71dfcebd1f40.

---
+++ app/src/main/java/com/example/pop/LoginActivity.java
@@ -0,0 +1,77 @@
+package com.example.pop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private TextView registerLink;
+    private TextView errorMsg;
+    private TextView attempts;
+    private Button loginBtn;
+    private int count = 4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        registerLink = findViewById(R.id.regLink);
+        errorMsg = findViewById(R.id.loginErrorMsg);
+        attempts = findViewById(R.id.attempts);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean loginDetailsMatch = validateMatch(email.getText().toString(), password.getText().toString());
+                if(loginDetailsMatch){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean validateMatch(String username, String userPassword) {
+        if((username.equals("admin")) && (userPassword.equals("admin"))){
+            return true;
+        }
+        else{
+            failedAttempt();
+            return false;
+        }
+    }
+
+    private void failedAttempt() {
+        count--;
+
+        errorMsg.setText("Wrong email or password!");
+        attempts.setText("No of attempts remaining: " + count);
+
+        if(count == 0)
+        {
+            loginBtn.setEnabled(false);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pop/RegisterActivity.java
@@ -0,0 +1,141 @@
+package com.example.pop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText email;
+    private EditText pass;
+    private EditText confirmPass;
+    private TextView loginLink;
+    private TextView errorMsg;
+    private Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.regUsername);
+        email = findViewById(R.id.regEmail);
+        pass = findViewById(R.id.regPassword);
+        confirmPass = findViewById(R.id.regConfirmPassword);
+        loginLink = findViewById(R.id.loginLink);
+        errorMsg = findViewById(R.id.regErrorMsg);
+        registerBtn = findViewById(R.id.regBtn);
+
+        username.addTextChangedListener(inputWatcher);
+        email.addTextChangedListener(inputWatcher);
+        pass.addTextChangedListener(inputWatcher);
+        confirmPass.addTextChangedListener(inputWatcher);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean validUsername = checkUsername(username.getText().toString());
+                boolean validEmail = checkEmail(email.getText().toString());
+                boolean validPassword = checkPassword(pass.getText().toString(), confirmPass.getText().toString());
+
+                if(validUsername && validEmail && validPassword) {
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private final TextWatcher inputWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        { }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {}
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (username.getText().toString().length() == 0 || email.getText().toString().length() == 0 ||
+                    pass.getText().toString().length() == 0 || confirmPass.getText().toString().length() == 0) {
+                registerBtn.setEnabled(false);
+            } else {
+                registerBtn.setEnabled(true);
+            }
+        }
+    };
+
+    private boolean checkUsername(String username) {
+        //Search db for username
+        //Return list named users
+        List<String> users = null;
+
+        //Check if username is taken
+        if(users == null){
+            Toast toast = Toast.makeText(getApplicationContext(), "Username already taken!", Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private boolean checkEmail(String email) {
+        //Search db for email
+        //Return list named users
+        List<String> users = null;
+
+        //Check if email is taken
+        if(users == null) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Email already taken!", Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean checkPassword(String pass, String confirmPass) {
+
+        Pattern passwordPattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+
+        if(!(passwordPattern.matcher(pass).matches())) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Password is too weak! \n Requires: lowercase, uppercase, numeric and special char", Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+        }
+        else if(!pass.equals(confirmPass)) {
+            System.out.println("Pass: " + pass);
+            System.out.println("Confirm Pass: " + confirmPass);
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Passwords don't match!", Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
